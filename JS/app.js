65873bbfef571239c1c399a07822bc2d82e1851a
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-04-11 15:30:36 +0300

    New lab06

---
+++ JS/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Seattle ={
+minCust : 23,
+maxCust : 65,
+avgCookies : 6.3 ,
+hours : [],
+custPerHour :[],
+cookieshuorly : [],
+
+hourWork:function(){
+for (let i=6; i< 20; i++){
+this.hours.push(i);
+this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
+// console.log(i);
+}},
+cookiesPerHour: function(){
+    for (let i=0 ; i<this.hours.length ; i++){
+    this.cookieshuorly.push(this.avgCookies*this.custPerHour[i]);
+    // console.log(this.cookieshuorly);
+}}
+};
+Seattle.hourWork();
+Seattle.cookiesPerHour();
+console.log(Seattle.custPerHour);
+
+// helper function
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
