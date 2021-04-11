a1ad008202a6db6b2bda4c9e5d52639e48c32046
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-04-11 18:07:39 +0300

    The latest editing for today

--- JS/app.js
+++ JS/app.js
@@ -1,121 +1,271 @@
 'use strict';
 
 const Seattle ={
-minCust : 23,
-maxCust : 65,
-avgCookies : 6.3 ,
-hours : [],
-custPerHour :[],
-cookieshuorly : [],
-
-hourWork:function(){
-for (let i=6; i< 20; i++){
-this.hours.push(i);
-this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
-// console.log(i);
-}},
-cookiesPerHour: function(){
-    for (let i=0 ; i<this.hours.length ; i++){
-    this.cookieshuorly.push(this.avgCookies*this.custPerHour[i]);
-    // console.log(this.cookieshuorly);
-}}
-};
-Seattle.hourWork();
-Seattle.cookiesPerHour();
-console.log(Seattle.custPerHour);
-
-// helper function
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-
-
-  const Tokyo ={
-    minCust : 3,
-    maxCust : 24,
-    avgCookies : 1.2 ,
+    minCust : 23,
+    maxCust : 65,
+    avgCookies : 6.3 ,
     hours : [],
     custPerHour :[],
     cookieshuorly : [],
     
     hourWork:function(){
     for (let i=6; i< 20; i++){
-    this.hours.push(i);
+        if (i < 12){
+            this.hours.push(`${i}am`);
+        }else if (i==12){
+            this.hours.push(`${i}pm`);
+        }
+        else{
+            this.hours.push(`${i-12}pm`)
+        }
     this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
     }},
+    
+    
     cookiesPerHour: function(){
         for (let i=0 ; i<this.hours.length ; i++){
-        this.cookieshuorly.push(this.avgCookies*this.custPerHour[i]);
-    }}
-    };
-    Tokyo.hourWork();
-    Tokyo.cookiesPerHour();
-    console.log(Tokyo.custPerHour);
+        this.cookieshuorly.push(Math.ceil(this.avgCookies*this.custPerHour[i]));
+      
+    }},
     
-    // helper function
+    
+    creatingList:function(){
+        for(let i=0; i<this.hours.length; i++){
+       let unorderlist =document.getElementById('myList');
+       let li = document.createElement('li');
+       li.textContent = `${this.hours[i]} , ${this.cookieshuorly[i]} cookies, This list in Seattle` 
+        unorderlist.appendChild(li);  
+    }
+//   console.log(Tokyo.cookiesPerHour);
+},
+    
+    };
+    Seattle.hourWork();
+    Seattle.cookiesPerHour();
+    Seattle.creatingList();
+  
+
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
       }
+    
+
+
+
+
+
+
+
+
+
+  const Tokyo ={
+    minCust : 23,
+    maxCust : 65,
+    avgCookies : 6.3 ,
+    hours : [],
+    custPerHour :[],
+    cookieshuorly : [],
+    
+    hourWork:function(){
+    for (let i=6; i< 20; i++){
+        if (i < 12){
+            this.hours.push(`${i}am`);
+        }else if (i==12){
+            this.hours.push(`${i}pm`);
+        }
+        else{
+            this.hours.push(`${i-12}pm`)
+        }
+    this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
+    }},
+    
+    
+    cookiesPerHour: function(){
+        for (let i=0 ; i<this.hours.length ; i++){
+        this.cookieshuorly.push(Math.ceil(this.avgCookies*this.custPerHour[i]));
+      
+    }},
+    
+    
+    creatingList:function(){
+        for(let i=0; i<this.hours.length; i++){
+       let unorderlist =document.getElementById('myList');
+       let li = document.createElement('li');
+       li.textContent = `${this.hours[i]} , ${this.cookieshuorly[i]} cookies, This list in Tokyo` 
+        unorderlist.appendChild(li);  
+    }
+  console.log(Tokyo.cookiesPerHour);
+},
+    
+    };
+    Tokyo.hourWork();
+    Tokyo.cookiesPerHour();
+    Tokyo.creatingList();
+  
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+      }
+    
 
       const Dubai ={
-        minCust : 3,
-        maxCust : 24,
-        avgCookies : 1.2 ,
+        minCust : 23,
+        maxCust : 65,
+        avgCookies : 6.3 ,
         hours : [],
         custPerHour :[],
         cookieshuorly : [],
         
         hourWork:function(){
         for (let i=6; i< 20; i++){
-        this.hours.push(i);
+            if (i < 12){
+                this.hours.push(`${i}am`);
+            }else if (i==12){
+                this.hours.push(`${i}pm`);
+            }
+            else{
+                this.hours.push(`${i-12}pm`)
+            }
         this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
         }},
+        
+        
         cookiesPerHour: function(){
             for (let i=0 ; i<this.hours.length ; i++){
-            this.cookieshuorly.push(this.avgCookies*this.custPerHour[i]);
-        }}
+            this.cookieshuorly.push(Math.ceil(this.avgCookies*this.custPerHour[i]));
+          
+        }},
+        
+        
+        creatingList:function(){
+            for(let i=0; i<this.hours.length; i++){
+           let unorderlist =document.getElementById('myList');
+           let li = document.createElement('li');
+           li.textContent = `${this.hours[i]} , ${this.cookieshuorly[i]} cookies,This list in Dubai` 
+            unorderlist.appendChild(li);  
+        }
+    //   console.log(Tokyo.cookiesPerHour);
+    },
+        
         };
         Dubai.hourWork();
         Dubai.cookiesPerHour();
-        console.log(Dubai.custPerHour);
-        
-        // helper function
+        Dubai.creatingList();
+      
+    
         function getRandomInt(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
           }
-
-
+        
+    
+    
           const Paris ={
-            minCust : 3,
-            maxCust : 24,
-            avgCookies : 1.2 ,
+            minCust : 23,
+            maxCust : 65,
+            avgCookies : 6.3 ,
             hours : [],
             custPerHour :[],
             cookieshuorly : [],
             
             hourWork:function(){
             for (let i=6; i< 20; i++){
-            this.hours.push(i);
+                if (i < 12){
+                    this.hours.push(`${i}am`);
+                }else if (i==12){
+                    this.hours.push(`${i}pm`);
+                }
+                else{
+                    this.hours.push(`${i-12}pm`)
+                }
             this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
             }},
+            
+            
             cookiesPerHour: function(){
                 for (let i=0 ; i<this.hours.length ; i++){
-                this.cookieshuorly.push(this.avgCookies*this.custPerHour[i]);
-            }}
+                this.cookieshuorly.push(Math.ceil(this.avgCookies*this.custPerHour[i]));
+              
+            }},
+            
+            
+            creatingList:function(){
+                for(let i=0; i<this.hours.length; i++){
+               let unorderlist =document.getElementById('myList');
+               let li = document.createElement('li');
+               li.textContent = `${this.hours[i]} , ${this.cookieshuorly[i]} cookies, This list in Paris` 
+                unorderlist.appendChild(li);  
+            }
+        //   console.log(Tokyo.cookiesPerHour);
+        },
+            
             };
             Paris.hourWork();
             Paris.cookiesPerHour();
-            console.log(Paris.custPerHour);
-            
-            // helper function
+            Paris.creatingList();
+          
+        
             function getRandomInt(min, max) {
                 min = Math.ceil(min);
                 max = Math.floor(max);
                 return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
               }
+            
+
+
+              const Lima ={
+                minCust : 23,
+                maxCust : 65,
+                avgCookies : 6.3 ,
+                hours : [],
+                custPerHour :[],
+                cookieshuorly : [],
+                
+                hourWork:function(){
+                for (let i=6; i< 20; i++){
+                    if (i < 12){
+                        this.hours.push(`${i}am`);
+                    }else if (i==12){
+                        this.hours.push(`${i}pm`);
+                    }
+                    else{
+                        this.hours.push(`${i-12}pm`)
+                    }
+                this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
+                }},
+                
+                
+                cookiesPerHour: function(){
+                    for (let i=0 ; i<this.hours.length ; i++){
+                    this.cookieshuorly.push(Math.ceil(this.avgCookies*this.custPerHour[i]));
+                  
+                }},
+                
+                
+                creatingList:function(){
+                    for(let i=0; i<this.hours.length; i++){
+                   let unorderlist =document.getElementById('myList');
+                   let li = document.createElement('li');
+                   li.textContent = `${this.hours[i]} , ${this.cookieshuorly[i]} cookies,  This list in Lima` 
+                    unorderlist.appendChild(li);  
+                }
+            //   console.log(Tokyo.cookiesPerHour);
+            },
+                
+                };
+                Lima.hourWork();
+                Lima.cookiesPerHour();
+                Lima.creatingList();
+              
+            
+                function getRandomInt(min, max) {
+                    min = Math.ceil(min);
+                    max = Math.floor(max);
+                    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+                  }
+                
