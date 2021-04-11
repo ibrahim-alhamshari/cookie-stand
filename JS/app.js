450201bb0183e90984db96c9b65ab11bba55e7fc
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-04-11 15:49:32 +0300

    The fourth editing

--- JS/app.js
+++ JS/app.js
@@ -89,4 +89,33 @@ function getRandomInt(min, max) {
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
           }
-    
+
+
+          const Paris ={
+            minCust : 3,
+            maxCust : 24,
+            avgCookies : 1.2 ,
+            hours : [],
+            custPerHour :[],
+            cookieshuorly : [],
+            
+            hourWork:function(){
+            for (let i=6; i< 20; i++){
+            this.hours.push(i);
+            this.custPerHour.push(getRandomInt(this.minCust, this.maxCust));
+            }},
+            cookiesPerHour: function(){
+                for (let i=0 ; i<this.hours.length ; i++){
+                this.cookieshuorly.push(this.avgCookies*this.custPerHour[i]);
+            }}
+            };
+            Paris.hourWork();
+            Paris.cookiesPerHour();
+            console.log(Paris.custPerHour);
+            
+            // helper function
+            function getRandomInt(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+              }
